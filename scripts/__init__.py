b6581e5e41ab26a1ccbdf7f21046d5cbf740ed4f
louisepaul75 <ricafortlouisepaul75@gmail.com>
2025-03-02 18:41:31 +0100

    linter issue

---
+++ scripts/__init__.py
@@ -0,0 +1,5 @@
+"""
+Scripts package for pyERP.
+
+This package contains various scripts for maintenance, development, and deployment tasks.
+""" 
---
+++ scripts/utilities/__init__.py
@@ -0,0 +1,5 @@
+"""
+Utilities package for pyERP scripts.
+
+This package contains utility scripts for various maintenance and development tasks.
+""" 
