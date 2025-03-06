14909d4e4d5be43ce65c673f462e91baf0eb7078
louisepaul75 <ricafortlouisepaul75@gmail.com>
2025-03-06 12:28:29 +0100

    Fix formatting in version.py

---
+++ pyerp/version.py
@@ -0,0 +1,32 @@
+"""Version information for pyERP."""
+
+__version__ = "0.1.0"
+
+
+def get_version():
+    """Return the current version."""
+    return __version__
+
+
+def bump_version(version_type="patch"):
+    """
+    Bump the version number.
+
+    Args:
+        version_type (str): Type of version bump ('major', 'minor', or 'patch')
+    """
+    global __version__
+    major, minor, patch = map(int, __version__.split("."))
+
+    if version_type == "major":
+        major += 1
+        minor = 0
+        patch = 0
+    elif version_type == "minor":
+        minor += 1
+        patch = 0
+    else:  # patch
+        patch += 1
+
+    __version__ = f"{major}.{minor}.{patch}"
+    return __version__
